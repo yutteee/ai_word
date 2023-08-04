c5f4026af9acc810db97efc4ada5654c6b252692
yutteee <yusaku_nakamura_aa@icloud.com>
2023-08-04 17:45:59 +0900

    create button component #4

---
+++ frontend/src/components/buttons/BaseButton/BaseButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface Props {
+  children?: React.ReactNode;
+  outline?: boolean;
+  handleClick?: () => void;
+}
+
+export const BaseButton: React.FC<Props> = ({ outline = false, children, handleClick }) => {
+  const style = outline
+    ? "btn border border-primary text-primary bg-white hover:bg-hover hover:border-primary transition-all"
+    : "btn bg-primary text-white hover:bg-hoverPrimary transition-all";
+
+  return (
+    <button className={style} onClick={handleClick}>
+      {children}
+    </button>
+  );
+};
